f357972cb52ee449aeaf35bbde518436ec8df782
Joonas Kajava <me@joonaskajava.com>
2021-07-12 22:03:12 +0300

    removed unnessessary console.log

--- webpack.plugins.js
+++ webpack.plugins.js
@@ -1,8 +1,6 @@
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 const webpack = require('webpack');
 
-console.dir(process.resourcesPath);
-
 module.exports = [
   new ForkTsCheckerWebpackPlugin(),
   new webpack.DefinePlugin({
